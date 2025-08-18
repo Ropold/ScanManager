80696ce170ed963cc622a0d497cc9eb753ab101a
robert.stolz <robert.stolz87@gmail.com>
2025-08-18 13:37:05 +0200

    ehandler clean code

--- backend/src/main/java/ropold/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/ropold/backend/exception/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package ropold.backend.exception;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ropold.backend.exception.notfoundexceptions.UserNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,16 +19,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler({
-            CustomerNotFoundException.class,
-            InvoiceNotFoundException.class,
-            LicenseNotFoundException.class,
-            ManufacturerNotFoundException.class,
             UserNotFoundException.class,
-            CustomerLicenseNotFoundException.class,
-            LicenseDependencyNotFoundException.class,
-            InvoiceBundleItemNotFoundException.class,
-            LicensePriceHistoryNotFoundException.class,
-            UserCustomerWishlistNotFound.class
     })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(RuntimeException e) {

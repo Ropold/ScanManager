6b00b4875ad9bb3441f0358e87eb058df7355c59
robert.stolz <robert.stolz87@gmail.com>
2025-08-22 12:25:12 +0200

    customer details beginning

--- frontend/src/components/CustomerDetails.tsx
+++ frontend/src/components/CustomerDetails.tsx
@@ -1,8 +1,55 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import type {CustomerModel} from "./model/CustomerModel.ts";
 
-export default function CustomerDetails() {
+type CustomerDetailsProps = {
+    language: string;
+}
+
+export default function CustomerDetails(props: Readonly<CustomerDetailsProps>) {
+    const [customer, setCustomer] = useState<CustomerModel | null>(null);
+    const {id} = useParams<{id: string}>();
+
+    useEffect(() => {
+        if(!id) return;
+        axios
+            .get(`/api/customer/${id}`)
+            .then((response) => setCustomer(response.data))
+            .catch((error) => console.error("Error fetching customer details", error));
+    }, [id]);
+
+
+
     return (
         <div>
             <h2>Customer Details</h2>
+            <p>{props.language}</p>
+        </div>
+    );
+}
+
+type CustomerDetailsProps = {
+    language: string;
+}
+
+export default function CustomerDetails(props: Readonly<CustomerDetailsProps>) {
+    const [customer, setCustomer] = useState<CustomerModel | null>(null);
+    const {id} = useParams<{id: string}>();
+
+    useEffect(() => {
+        if(!id) return;
+        axios
+            .get(`/api/customer/${id}`)
+            .then((response) => setCustomer(response.data))
+            .catch((error) => console.error("Error fetching customer details", error));
+    })[id]
+
+
+
+    return (
+        <div>
+            <h2>Customer Details</h2>
+            <p>{props.language}</p>
         </div>
     );
 }

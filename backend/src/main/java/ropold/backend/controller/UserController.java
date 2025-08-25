4722f571a089f0189d639fd42527125bfa49009c
robert.stolz <robert.stolz87@gmail.com>
2025-08-25 15:47:21 +0200

    prefered language backend

--- backend/src/main/java/ropold/backend/controller/UserController.java
+++ backend/src/main/java/ropold/backend/controller/UserController.java
@@ -4,9 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ropold.backend.exception.notfoundexceptions.AccessDeniedException;
 import ropold.backend.model.UserModel;
 import ropold.backend.service.UserService;
 
@@ -25,20 +24,31 @@ public class UserController {
     }
 
     @GetMapping("/me/details")
-    public Map<String, Object> getUserDetails(@AuthenticationPrincipal OAuth2User azureUser) {
-        if (azureUser == null) {
+    public Map<String, Object> getUserDetails(@AuthenticationPrincipal OAuth2User authentication) {
+        if (authentication == null) {
             return Map.of("message", "User not authenticated");
         }
-        UserModel user = userService.createOrUpdateFromAzure(azureUser);
+        UserModel user = userService.createOrUpdateFromAzure(authentication);
         return Map.of(
                 "id", user.getId(),
                 "microsoftId", user.getMicrosoftId(),
                 "username", user.getUsername(),
                 "email", user.getEmail(),
                 "role", user.getRole(),
+                "preferredLanguage", user.getPreferredLanguage(),
                 "avatarUrl", user.getAvatarUrl() != null ? user.getAvatarUrl() : "",
                 "createdAt", user.getCreatedAt() != null ? user.getCreatedAt().toString() : "",
                 "lastLoginAt", user.getLastLoginAt() != null ? user.getLastLoginAt().toString() : ""
         );
     }
+
+    @PostMapping("me/language/{languageIso}")
+    public void setPreferredLanguage(@PathVariable String languageIso, @AuthenticationPrincipal OAuth2User authentication) {
+        if (authentication == null) {
+            throw new AccessDeniedException("User not authenticated");
+        }
+        String microsoftId = authentication.getAttribute("sub");
+        userService.setPreferredLanguage(microsoftId, languageIso);
+    }
+
 }

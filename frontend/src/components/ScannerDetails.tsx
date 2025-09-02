128570b91cf9f06e0fc91fd6387bb89892169505
robert.stolz <robert.stolz87@gmail.com>
2025-09-02 10:36:13 +0200

    delete scanner works

--- frontend/src/components/ScannerDetails.tsx
+++ frontend/src/components/ScannerDetails.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {DefaultScanner, type ScannerModel} from "./model/ScannerModel.ts";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import {translatedInfo} from "./utils/TranslatedInfo.ts";
 import type {CustomerModel} from "./model/CustomerModel.ts";
@@ -13,11 +13,14 @@ type ScannerDetailsProps = {
 
     allArchivedCustomer: CustomerModel[];
     allArchivedServicePartner: ServicePartnerModel[];
+    handleScannerDelete: (id: string) => void;
 }
 
 export default function ScannerDetails(props: Readonly<ScannerDetailsProps>) {
     const [scanner, setScanner]= useState<ScannerModel>(DefaultScanner)
     const {id} = useParams<{id: string}>();
+    const [showPopup, setShowPopup] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if(!id) return;
@@ -46,6 +49,28 @@ export default function ScannerDetails(props: Readonly<ScannerDetailsProps>) {
             .put(`/api/scanners/${scanner.id}/archive`)
             .then((response) => setScanner(response.data))
             .catch((error) => console.error("Error updating archive status", error));
+    }
+
+    function handleConfirmDelete(){
+        if(!scanner?.id) return;
+
+        axios
+            .delete(`/api/scanners/${scanner.id}`)
+            .then(() => {
+                console.log("Scanner deleted successfully");
+                props.handleScannerDelete(scanner.id); // Liste aktualisieren
+            })
+            .catch((error) => {
+                console.error("Error deleting Scanner:", error);
+                alert("An unexpected error occurred. Please try again.");
+            })
+            .finally(() => {
+                setShowPopup(false);
+                navigate("/scanners/");
+            });
+    }
+    function handleCancel(){
+        setShowPopup(false);
     }
 
     return(
@@ -92,8 +117,22 @@ export default function ScannerDetails(props: Readonly<ScannerDetailsProps>) {
                     <div className="details-buttons">
                         <button className="button-blue">Edit</button>
                         <button className="button-grey" onClick={toggleArchiveStatus}>{scanner.isArchived ? "Unarchive" : "Archive"}</button>
-                        <button className="button-delete">Delete</button>
+                        <button className="button-delete" onClick={() => setShowPopup(true)}>Delete</button>
                     </div>
+
+                    {showPopup && (
+                        <div className="popup-overlay">
+                            <div className="popup-content">
+                                <h3>Confirm Deletion</h3>
+                                <p>Are you sure you want to delete this scanner?</p>
+                                <div className="popup-actions">
+                                    <button onClick={handleConfirmDelete} className="popup-confirm">Yes, Delete</button>
+                                    <button onClick={handleCancel} className="popup-cancel">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
                 </div>
             ) : (
                 <p>{translatedInfo["Loading"][props.language]}...</p>
